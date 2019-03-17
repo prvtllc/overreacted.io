c2084c665ea3b4e566999a0352076fedd7d7e8b7
Domingo Rivera <djr@infosecusa.com>
2019-03-16 20:52:18 -0400

    e

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -24,8 +24,8 @@ class Bio extends React.Component {
         />
         <p style={{ maxWidth: 310 }}>
           Personal blog by{' '}
-          <a href="https://mobile.twitter.com/dan_abramov">Dan Abramov</a>.{' '}
-          I&nbsp;explain with words and code.
+          <a href="https://mobile.twitter.com/">D3fcon</a>.{' '}
+          I&nbsp;security news and information.
         </p>
       </div>
     );
