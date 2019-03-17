dbf6830d6c21737126f248d9c3ea782e769212bd
Domingo Rivera <djr@infosecusa.com>
2019-03-16 23:18:26 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,8 +43,8 @@ exports.createPages = ({ graphql, actions }) => {
       });
     });
 
-    resolve(
-      graphql(
+//    resolve(
+//      graphql(
         `
           {
             allMarkdownRemark(
@@ -85,95 +85,95 @@ exports.createPages = ({ graphql, actions }) => {
           new Set()
         );
 
-        const translationsByDirectory = _.reduce(
-          posts,
-          (result, post) => {
-            const directoryName = _.get(post, 'node.fields.directoryName');
-            const langKey = _.get(post, 'node.fields.langKey');
+      //  const translationsByDirectory = _.reduce(
+      //    posts,
+      //    (result, post) => {
+      //      const directoryName = _.get(post, 'node.fields.directoryName');
+      //      const langKey = _.get(post, 'node.fields.langKey');
 
-            if (directoryName && langKey && langKey !== 'en') {
-              (result[directoryName] || (result[directoryName] = [])).push(
-                langKey
-              );
-            }
+     //       if (directoryName && langKey && langKey !== 'en') {
+     //         (result[directoryName] || (result[directoryName] = [])).push(
+     //           langKey
+     //         );
+     //       }
 
-            return result;
-          },
-          {}
-        );
+    //        return result;
+    //      },
+    //      {}
+    //    );
 
-        const defaultLangPosts = posts.filter(
-          ({ node }) => node.fields.langKey === 'en'
-        );
-        _.each(defaultLangPosts, (post, index) => {
-          const previous =
-            index === defaultLangPosts.length - 1
-              ? null
-              : defaultLangPosts[index + 1].node;
-          const next = index === 0 ? null : defaultLangPosts[index - 1].node;
+    //    const defaultLangPosts = posts.filter(
+    //      ({ node }) => node.fields.langKey === 'en'
+    //    );
+    //    _.each(defaultLangPosts, (post, index) => {
+    //      const previous =
+    //        index === defaultLangPosts.length - 1
+    //          ? null
+    //          : defaultLangPosts[index + 1].node;
+    //      const next = index === 0 ? null : defaultLangPosts[index - 1].node;
 
-          const translations =
-            translationsByDirectory[_.get(post, 'node.fields.directoryName')] ||
-            [];
+    //      const translations =
+    //        translationsByDirectory[_.get(post, 'node.fields.directoryName')] ||
+    //        [];
 
-          createPage({
-            path: post.node.fields.slug,
-            component: blogPost,
-            context: {
-              slug: post.node.fields.slug,
-              previous,
-              next,
-              translations,
-              translatedLinks: [],
-            },
-          });
+    //      createPage({
+    //        path: post.node.fields.slug,
+    //        component: blogPost,
+    //        context: {
+    //          slug: post.node.fields.slug,
+    //          previous,
+    //          next,
+    //          translations,
+    //          translatedLinks: [],
+    //        },
+    //      });
 
-          const otherLangPosts = posts.filter(
-            ({ node }) => node.fields.langKey !== 'en'
-          );
-          _.each(otherLangPosts, post => {
-            const translations =
-              translationsByDirectory[_.get(post, 'node.fields.directoryName')];
+    //      const otherLangPosts = posts.filter(
+    //        ({ node }) => node.fields.langKey !== 'en'
+    //      );
+    //      _.each(otherLangPosts, post => {
+    //        const translations =
+    //          translationsByDirectory[_.get(post, 'node.fields.directoryName')];
 
             // Record which links to internal posts have translated versions
             // into this language. We'll replace them before rendering HTML.
-            let translatedLinks = [];
-            const { langKey, maybeAbsoluteLinks } = post.node.fields;
-            maybeAbsoluteLinks.forEach(link => {
-              if (allSlugs.has(link)) {
-                if (allSlugs.has('/' + langKey + link)) {
+    //        let translatedLinks = [];
+    //        const { langKey, maybeAbsoluteLinks } = post.node.fields;
+    //        maybeAbsoluteLinks.forEach(link => {
+    //          if (allSlugs.has(link)) {
+    //            if (allSlugs.has('/' + langKey + link)) {
                   // This is legit an internal post link,
                   // and it has been already translated.
-                  translatedLinks.push(link);
-                } else if (link.startsWith('/' + langKey + '/')) {
-                  console.log('-----------------');
-                  console.error(
-                    `It looks like "${langKey}" translation of "${
-                      post.node.frontmatter.title
-                    }" ` +
-                      `is linking to a translated link: ${link}. Don't do this. Use the original link. ` +
-                      `The blog post renderer will automatically use a translation if it is available.`
-                  );
-                  console.log('-----------------');
-                }
-              }
-            });
+    //              translatedLinks.push(link);
+    //            } else if (link.startsWith('/' + langKey + '/')) {
+    //              console.log('-----------------');
+    //              console.error(
+    //                `It looks like "${langKey}" translation of "${
+    //                  post.node.frontmatter.title
+    //                }" ` +
+    //                  `is linking to a translated link: ${link}. Don't do this. Use the original link. ` +
+    //                  `The blog post renderer will automatically use a translation if it is available.`
+    //              );
+    //              console.log('-----------------');
+   //            }
+   //           }
+   //         });
 
-            createPage({
-              path: post.node.fields.slug,
-              component: blogPost,
-              context: {
-                slug: post.node.fields.slug,
-                translations,
-                translatedLinks,
-              },
-            });
-          });
-        });
-      })
-    );
-  });
-};
+  //          createPage({
+  //            path: post.node.fields.slug,
+  //            component: blogPost,
+  //            context: {
+  //              slug: post.node.fields.slug,
+  //              translations,
+  //              translatedLinks,
+  //            },
+  //          });
+  //        });
+  //      });
+  //    })
+  //  );
+ // });
+//};
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
